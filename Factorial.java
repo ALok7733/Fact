f2df7f9a22e0740e8b06b5979abf6b3c28d1ee8c
Alok <118507251+ALok7733@users.noreply.github.com>
2022-11-23 12:42:08 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,22 +1,18 @@
-
+import java.util.Scanner;
 
 public class Factorial {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		Factorial.getFact();
-	}
-
-	
-	public static void getFact() {
 		
-		int num = 5;
-		int fact = 1;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enetr a number for which you need factorial");
+		int num = sc.nextInt();
+		int res = 1;
 		for(int i=1;i<=num;i++) {
-			fact = fact * i;
-			
+			res = res * i;
 		}
-		System.out.println("Factorial of:  "+num+ " is: = "+fact);
+		System.out.println("Result: "+res);
 	}
+
 }
